18229af4974a15c76677b71bd17eb7ea3da5eacd
hiendinhngoc <hiendn@bitnemo.vn>
2015-03-26 19:41:30 +0700

    update fixing facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider	:facebook,
-				ENV['809207902448757'], ENV['552eb4be4bc19111712da4420a31b3e2'],
+				'809207902448757', '552eb4be4bc19111712da4420a31b3e2',
 				scope: 'public_profile', display: 'page', image_size: 'square'
 end
