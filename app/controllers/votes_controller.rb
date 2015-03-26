426e043564213c0764446c85af7784dca14eb89d
hiendinhngoc <hiendn@bitnemo.vn>
2015-03-27 01:36:37 +0700

    update error shared/show

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,7 @@
 		if current_user && params[:poll] && params[:poll][:id] && params[:vote_option] && params[:vote_option][:id]
 			@poll = Poll.find_by_id(params[:poll][:id])
 			@option = @poll.vote_options.find_by_id(params[:vote_option][:id])
-			if @option && @poll && !current_user.vote_for?(@poll)
+			if @option && @poll && !current_user.voted_for?(@poll)
 				current_user.votes.create({vote_option_id: @option.id})			
 			else
 				render js: 'alert(\'Your vote cannot be saved. Have you already voted?\');'
