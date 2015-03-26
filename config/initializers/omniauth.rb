0ec3ff20c8d9254c2b10e4bc5c94c5a52eaff17e
hiendinhngoc <hiendn@bitnemo.vn>
2015-03-26 19:26:09 +0700

    update fixing css

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider	:facebook,
-				ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+				ENV['809207902448757'], ENV['552eb4be4bc19111712da4420a31b3e2'],
 				scope: 'public_profile', display: 'page', image_size: 'square'
 end
