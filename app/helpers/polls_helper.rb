924150f5fecfb7efb292ca8d2ac60945c10f96c6
hiendinhngoc <hiendn@bitnemo.vn>
2015-03-28 16:06:05 +0700

    fixed piechart

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -3,8 +3,7 @@ module PollsHelper
 		content_tag :div, class: 'progress' do
 			content_tag :div, class: 'progress-bar',
 					style: "width: #{option.poll.normalized_votes_for(option)}%" do
-				
-					pie_chart VoteOption.group(:title).sum("#{option.votes.count}")
+				"#{option.votes.count}"
 			end
 		end
 	end
