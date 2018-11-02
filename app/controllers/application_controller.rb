dd9c4fde997f2054e72e8e3509365982d16a84a1
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2018-11-02 16:37:52 +0700

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,5 @@ class ApplicationController < ActionController::Base
   	@current_user ||= User.find_by(id: cookies[:user_id]) if cookies[:user_id]
   end
 
-  p "helo"
-
   helper_method :current_user
 end
